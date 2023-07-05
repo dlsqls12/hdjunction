053e7922030d79c2182c25ea75a77e6e71d8fef8
INBIN JANG <dlsqls12@naver.com>
2023-07-05 11:21:46 +0900

    스토리지 저장을 위해 redux-persist  적용

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import persistReducer from 'redux-persist/es/persistReducer';
+import storage from 'redux-persist/lib/storage';
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import headerSlice from './main/headerSlice';
 import contentSlice from './main/contentSlice';
 
-export const store = configureStore({
-  reducer: {
-    header: headerSlice,
-    content: contentSlice
-  },
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const rootReducer = combineReducers({
+  header: headerSlice,
+  content: contentSlice
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
+});
+
+export default store;
