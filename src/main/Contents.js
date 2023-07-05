b6e33b00065f711f678488581ba5ec272c3c35e4
INBIN JANG <dlsqls12@naver.com>
2023-07-05 09:13:29 +0900

    사각형 그리기

--- src/main/Contents.js
+++ src/main/Contents.js
@@ -1,40 +1,98 @@
+import { useEffect, useState } from 'react';
+
 function Contents() {
-  let isDraw = false;
-  const mouseDown = (event) => {
-    console.log('mouseDown');
-    isDraw = true;
-    console.log(event);
-  }
-  const mouseMove = (event) => {           
-    if (isDraw) {
-      console.log('mouseMove');
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [drawObj, setDrawObj] = useState({});
+  const [drawList, setDrawList] = useState(() => {
+    const localDrawList = localStorage.getItem('drawList');
+    if (localDrawList) {
+      return JSON.parse(localDrawList);
+
+    } else {
+      return [];
     }
+  });
+
+  useEffect(() => {
+    localStorage.setItem('drawList', JSON.stringify(drawList));
+  }, [drawList]);
+
+  // 그리기 시작
+  const mouseDown = (event) => {
+    if (!isDrawing) {
+      setDrawObj({
+        orgY: event.pageY
+        , orgX: event.pageX
+        , style: {
+          position: 'absolute'
+          , border: '1px solid #aaa'
+          , top: event.pageY
+          , left: event.pageX
+          , width: '1px'
+          , height: '1px'
+        }
+      });
+      setIsDrawing(true);
+    }
+    event.preventDefault();
   }
+
+  // 그리는 중
+  const mouseMove = (event) => {
+    if (isDrawing) {
+      const pageY = event.pageY, pageX = event.pageX;
+      const orgX = drawObj.orgX, orgY = drawObj.orgY;
+      let width, height, left, top;
+
+      if (pageX >= orgX) {
+        width = pageX - orgX;
+        left = drawObj.style.left
+
+      } else {
+        width = orgX - pageX;
+        left = pageX;
+      }
+
+      if (pageY >= orgY) {
+        height = pageY - orgY;
+        top = drawObj.style.top
+
+      } else {
+        height = orgY - pageY;
+        top = pageY;
+      }
+
+      setDrawObj({
+        ...drawObj,
+        style: {
+          ...drawObj.style,
+          top: top,
+          left: left,
+          width: width + 'px',
+          height: height + 'px'
+        }
+      });
+    }
+    event.preventDefault();
+  }
+
+  // 그리기 끝
   const mouseUp = (event) => {
-    console.log('mouseUp');
-    isDraw = false;
-    console.log(event);
+    if (isDrawing) {
+      setIsDrawing(false);
+
+      setDrawList([...drawList, drawObj]);
+      setDrawObj({});
+    }
+    event.preventDefault();
   }
 
   return (
     <div className="contents" onMouseDown={mouseDown} onMouseMove={mouseMove} onMouseUp={mouseUp}>
-      <div style={{
-        position: 'absolute'
-        , border: '1px solid #aaa'
-        , borderRadius: '50%'
-        , top: '0px'
-        , left: '0px'
-        , width: '200px'
-        , height: '140px'
-      }}></div>
-      <div style={{
-        position: 'absolute'
-        , border: '1px solid #aaa'
-        , top: '300px'
-        , left: '350px'
-        , width: '200px'
-        , height: '140px'
-      }}></div>
+      {drawList.map((item, index) => (
+        <div key={index} style={item.style}/>
+      ))}
+      <div key="selected" style={drawObj.style} />
     </div>
   );
 }
